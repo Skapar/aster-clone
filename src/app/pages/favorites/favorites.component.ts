bc8e43f1317365e87e6989aa270079b64a77c083
Your New Username <yournewemail@example.com>
2024-10-19 08:24:47 +0500

    fix: add header and footer for favorites and tires pages

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { CommonModule, NgFor } from '@angular/common';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-favorites',
+  standalone: true,
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.css']
+  styleUrls: ['./favorites.component.css'],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    NgFor,
+
+  ]
 })
 export class FavoritesComponent {
   onEnter() {
