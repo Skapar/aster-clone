e39f93ec99977940ce420e57a5f94bb289a07b2d
abdusss111 <abdusalamabdu29@gmail.com>
2024-12-07 02:58:15 +0500

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,9 @@ import { provideRouter } from '@angular/router';
 import { AuthInterceptor} from './components/auth/auth.interceptor';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {importProvidersFrom} from '@angular/core';
+import {importProvidersFrom, isDevMode} from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient} from "@angular/common/http";
+import { provideServiceWorker } from '@angular/service-worker';
 
 
 export const appConfig: ApplicationConfig = {
@@ -19,6 +20,9 @@ export const appConfig: ApplicationConfig = {
         multi: true
       }
     ],
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync('noop')],
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync('noop'), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })],
 
 };
