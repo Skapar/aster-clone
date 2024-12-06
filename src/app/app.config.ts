df4ccc0904699202c00ebf684431cc6eaee2d7c6
abdusss111 <abdusalamabdu29@gmail.com>
2024-12-07 02:49:51 +0500

    jwt auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { AuthInterceptor} from './components/auth/auth.interceptor';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {importProvidersFrom} from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient} from "@angular/common/http";
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync('noop')]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(HttpClientModule),
+    [
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptor,
+        multi: true
+      }
+    ],
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync('noop')],
+
 };
--- src/app/components/auth/auth-component.component.ts
+++ src/app/components/auth/auth-component.component.ts
@@ -1,13 +1,53 @@
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { RouterModule} from '@angular/router';
+import { AuthenticationService} from './auth.service';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule, RouterModule
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth-component.css'
 })
-export class AuthComponent {}
+export class AuthComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private as: AuthenticationService,
+              private router: Router
+  ) {
+  }
+  login() {
+
+    console.log(this.password, this.username);
+    this.as
+      .signIn(this.username, this.password)
+      .subscribe({
+        next: (data) => {
+          localStorage.setItem("access", data.access);
+          localStorage.setItem("refresh", data.refresh);
+          alert("You have successfully logged in")
+          this.router.navigate(['/home']); // Redirect to the 'dashboard' page
+          console.log("Login successful!");
+        },
+        error: (err) => {
+          console.error("Login failed:", err);
+          if (err.status === 401) {
+            alert("Invalid username or password. Please try again.");
+          } else if (err.status === 0) {
+            alert("Unable to connect to the server. Please check your internet connection.");
+          } else {
+            alert("An unexpected error occurred. Please try again later.");
+          }
+        },
+      });
+    this.password = '';
+    this.username = '';
+  }
+
+
+}
